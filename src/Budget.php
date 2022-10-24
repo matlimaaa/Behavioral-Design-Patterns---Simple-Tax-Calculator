09cf6d5efe3a636cce6285914b60b20a1ea1b101
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2022-10-24 19:28:51 -0300

    feat: Created rule for extra discounts on in-progress and approved budget stages

--- src/Budget.php
+++ src/Budget.php
@@ -2,8 +2,31 @@
 
 namespace Src\DesignPattern;
 
+use DomainException;
+
 class Budget
 {
     public float $value;
     public int $quantityOfItems;
+    public string $currentState;
+
+    public function applyExtraDiscount()
+    {
+        $this->value -= $this->calculateExtraDiscount();
+    }
+
+    public function calculateExtraDiscount(): float
+    {
+        if ($this->currentState === 'IN_PROGRESS') {
+            return $this->value * 0.05;
+        }
+
+        if ($this->currentState === 'APPROVED') {
+            return $this->value * 0.02;
+        }
+
+        throw new DomainException(
+            'Failed and completed budget cannot receive discounts'
+        );
+    }
 }
